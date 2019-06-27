6c748d27e2e44808903c23e26aa68319b338b056
Bipin Radhakrishnan <hi@bipinr.com>
2019-06-27 00:14:50 -0400

    Adding comments and Readme

--- AutoMapper/AutoMapper.cs
+++ AutoMapper/AutoMapper.cs
@@ -10,12 +10,12 @@ namespace AutoMapper
 {
     public static class AutoMapper
     {
-        //static bool preserveExisting = false;
         /// <summary>
-        /// Map the source type to the destination property, this method can scan 3 levels deep and map automatically if the type is unique
-        /// Scan is restricted to only 3 levels down to handle larger/ complicated objects that could have multiple properties of same type.
+        /// Map the source type to the destination property, this can go N levels deeper and try to man the source type to the target
+        /// In case of objects with multiple properties that are of same type, this method will map to the first property it finds.
+        /// 
         /// </summary>
-        /// <param name="destination">Destination object on which the extension method is called</param>
+        /// <param name="destination">Targer object on which the extension method is called</param>
         /// <param name="source">Source object that will be mapped</param>
         public static void Map(this object destination, object source, bool preserveExistingValue = false)
         {
@@ -25,12 +25,14 @@ namespace AutoMapper
             if (source == null)
                 throw new NullArgumentException("The source object (object from which the mapping has to happen) is null",
                     $"{GetCurrentMethodName()}");
+            
+            //Get the source and target object type
             var destinationType = destination.GetType();
             var sourceType = source.GetType();
 
             //If the destinaton type and source type are same and
             //destination is a list or array, then just assign source
-
+            //Locate the correct property and instance on which the mapping has to happen
             TrieNodeProperty destinationNode = LocateSourcePropertyTypeInDestination(destination, source);
 
             if (sourceType.IsGenericType || sourceType.IsArray)
@@ -58,34 +60,40 @@ namespace AutoMapper
                                                     TrieNodeProperty sourcePropertyNodes,
                                                     bool preserveExistingValue = false)
         {
+            //Get the top most node in the tree
             TrieNodeProperty topMostNode = sourcePropertyNodes.GetTopMostRoot();
-
+            //Map the source to the destination
             MapSourceToDestination(destinationNode, topMostNode);
 
         }
 
         /// <summary>
-        /// 
+        /// Go over the properties and map them to the target (destination)
         /// </summary>
-        /// <param name="destinationNode"></param>
-        /// <param name="sourcePropertyNode"></param>
-        private static void MapSourceToDestination(TrieNodeProperty destinationNode, TrieNodeProperty sourcePropertyNode)
+        /// <param name="targetTrieNode">Target Trie node</param>
+        /// <param name="sourcePropertyNode">Source Trie Node</param>
+        private static void MapSourceToDestination(TrieNodeProperty targetTrieNode, TrieNodeProperty sourcePropertyNode)
         {
-            if (destinationNode.Instance == null)
+           //If the target Instance is null, that mean the property is null
+            if (targetTrieNode.Instance == null)
             {
-                var parentInstance = destinationNode.GetParent().Instance;
-                AssignSourceToDestination(parentInstance, parentInstance, sourcePropertyNode.Instance, destinationNode.Property);
+                //get the parent instance and assign the source directly to that.
+                var parentInstance = targetTrieNode.GetParent().Instance;
+                AssignSourceToDestination(parentInstance, parentInstance, sourcePropertyNode.Instance, targetTrieNode.Property);
                 return;
             }
+
             if (sourcePropertyNode.HasChildren())
             {
+                //Since the property has children, then we go deeper perform mapping on them
+                //Iterate over each sub property and perform the mapping
                 sourcePropertyNode.Children.ForEach(childProperty =>
                 {
-                    var targetChildProperty = destinationNode.Instance.GetType().GetProperty(childProperty.Property.Name);
-                    var targetPropertyInstance = targetChildProperty.GetValue(destinationNode.Instance);
+                    var targetChildProperty = targetTrieNode.Instance.GetType().GetProperty(childProperty.Property.Name);
+                    var targetPropertyInstance = targetChildProperty.GetValue(targetTrieNode.Instance);
                     if (targetPropertyInstance == null)
                     {
-                        AssignSourceToDestination(destinationNode.Instance, destinationNode.Instance, childProperty.Instance, targetChildProperty);
+                        AssignSourceToDestination(targetTrieNode.Instance, targetTrieNode.Instance, childProperty.Instance, targetChildProperty);
                         return;
                     }
                     var targetNode = new TrieNodeProperty(null, targetChildProperty, targetPropertyInstance);
@@ -96,10 +104,12 @@ namespace AutoMapper
             }
             else
             {
+                //Else - this means the property has no more child properties or you have reached the last property.
+                //Map directly
                 var sourceProperty = sourcePropertyNode.Property;
 
-                var targetChildProperty = destinationNode.Instance.GetType().GetProperty(sourceProperty.PropertyType.Name);
-                var targetPropertyInstance = targetChildProperty.GetValue(destinationNode.Instance);
+                var targetChildProperty = targetTrieNode.Instance.GetType().GetProperty(sourceProperty.PropertyType.Name);
+                var targetPropertyInstance = targetChildProperty.GetValue(targetTrieNode.Instance);
 
                 AssignSourceToDestination(targetPropertyInstance, targetPropertyInstance, sourcePropertyNode.Instance, targetChildProperty);
 
@@ -107,19 +117,23 @@ namespace AutoMapper
         }
 
         /// <summary>
-        /// 
+        /// This method takes care of assigning the value to the targer object
         /// </summary>
         /// <param name="destinationInstance"></param>
         /// <param name="sourceIntance"></param>
         /// <param name="destinationProperty"></param>
-        private static void AssignSourceToDestination(object destination, object destinationData, object sourceIntance, PropertyInfo destinationProperty)
+        private static void AssignSourceToDestination(object target, 
+                                                      object targetData, 
+                                                      object sourceIntance, 
+                                                      PropertyInfo destinationProperty)
         {
-            if (destinationProperty.PropertyType.IsGenericType)
-                sourceIntance = CopyOverList(destinationData, sourceIntance);
-            else if (destinationProperty.PropertyType.IsArray)
-                sourceIntance = CopyOverArray(destinationData, sourceIntance);
-
-            destinationProperty.SetValue(destination, sourceIntance);
+            
+            if (destinationProperty.PropertyType.IsGenericType) //Property is a list
+                sourceIntance = CopyOverList(targetData, sourceIntance);
+            else if (destinationProperty.PropertyType.IsArray) //Property is an array
+                sourceIntance = CopyOverArray(targetData, sourceIntance);
+            //Set the value
+            destinationProperty.SetValue(target, sourceIntance);
         }
 
         /// <summary>
@@ -131,21 +145,22 @@ namespace AutoMapper
         /// <param name="propertyMap">Property Node</param>
         /// <param name="propertyDepth">Current Depth of the scan</param>
         private static void GetProperties(PropertyInfo property,
-            string parentPropertyName,
-            Dictionary<Type, Dictionary<string, TrieNodeProperty>> propertyMappingIndex,
-            TrieNodeProperty parentProperty)
+                                          string parentPropertyName,
+                                          Dictionary<Type, Dictionary<string, TrieNodeProperty>> propertyMappingIndex,
+                                          TrieNodeProperty parentProperty)
         {
             string propertyName = string.Empty;
-            object propertInstance = null;
+            object propertyInstance = null;
 
-            //Create the object TRIE
+            //Check the instance of parent property
             if (parentProperty.Instance != null)
-                propertInstance = property.GetValue(parentProperty.Instance);
-            else
-                propertInstance = null;
+                propertyInstance = property.GetValue(parentProperty.Instance);
 
-            parentProperty = new TrieNodeProperty(parentProperty, property, propertInstance);
+            //Create the TRIE node
+            parentProperty = new TrieNodeProperty(parentProperty, property, propertyInstance);
+
             propertyName = parentPropertyName.ToUpper();
+            //Start indexing the property
             if (propertyMappingIndex.ContainsKey(property.PropertyType))
             {
                 var propertiesList = propertyMappingIndex[property.PropertyType];
@@ -171,7 +186,11 @@ namespace AutoMapper
             }
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="propertyMap"></param>
         private static void GetSubProperties(PropertyInfo property,
            TrieNodeProperty propertyMap)
         {
@@ -181,12 +200,13 @@ namespace AutoMapper
             //Map the Sub properties
             if (IsClass(property))
             {
-
+                //Get all the properties that are public
                 var subProperties = property.PropertyType
                     .GetProperties(BindingFlags.Public | BindingFlags.Instance)
                     .Where(x => !x.GetMethod.IsPrivate
                         && !x.SetMethod.IsPrivate)
                     .ToList();
+
                 //Get the could of properties that are public
                 var publicPropertiesCount = subProperties.Count;
 
@@ -209,7 +229,7 @@ namespace AutoMapper
                 if (publicPropertiesCount != filteredPublicPropertiesCount)
                     propertyMap.PreserveExisting = true;
 
-
+                //Iterate the sub properties
                 subProperties.ForEach(subproperty =>
                 {
                     GetSubProperties(subproperty, propertyMap);
@@ -256,11 +276,11 @@ namespace AutoMapper
         /// <param name="sourceNode"></param>
         /// <param name="propertyName"></param>
         /// <returns></returns>
-        private static TrieNodeProperty LocateSourcePropertyTypeInDestination(object destination, object sourceNode, string propertyName = "")
+        private static TrieNodeProperty LocateSourcePropertyTypeInDestination(object target, object sourceNode, string propertyName = "")
         {
             Dictionary<Type, Dictionary<string, TrieNodeProperty>> destinationPropertyIndex =
                 new Dictionary<Type, Dictionary<string, TrieNodeProperty>>();
-            destinationPropertyIndex = LoadDestinationPropertyMap(destination);
+            destinationPropertyIndex = LoadDestinationPropertyMap(target);
             TrieNodeProperty correctProperty = null;
 
             var sourceType = sourceNode.GetType();
@@ -276,13 +296,11 @@ namespace AutoMapper
                 if (string.IsNullOrEmpty(propertyName))
                 {
                     var propertyNode = destinationPropertyCollection.Values.First();
-                    //correctProperty = new TrieNodeProperty(null, propertyNode.Property, propertyNode.Instance);
                     return propertyNode;
                 }
                 else
                 {
                     var propertyNode = destinationPropertyCollection[propertyName];
-                    //correctProperty = new TrieNodeProperty(null, propertyNode.Property, propertyNode.Instance);
                     return propertyNode;
                 }
             }
@@ -391,65 +409,11 @@ namespace AutoMapper
         }
 
         /// <summary>
-        /// Map method override to pass the property name, use this if there are multiple properties with same type,
-        /// provide the property name in the format of "ParentProperty.ChildProperty" ChildProperty is the target you want to map the type to, this scan only till
-        /// 3 levels deep to look for the property and maps them automatically.
+        /// Copy over an Array, if the array already has items, add them to the array
         /// </summary>
-        /// <param name="destination">Destination object on which the extension method is called</param>
-        /// <param name="source">Source object to map to the destination</param>
-        /// <param name="propertyName">Name of the property to Map it to</param>
-        //public static void Map(this object destination,
-        //    object source,
-        //    string propertyName,
-        //    bool preserveExistingValue = false)
-        //{
-        //    if (destination == null)
-        //        throw new NullArgumentException("The desination object (object to which the mapping has to happen) is null",
-        //            $"{GetCurrentMethodName()}");
-        //    if (source == null)
-        //        throw new NullArgumentException("The source object (object from which the mapping has to happen) is null",
-        //            $"{GetCurrentMethodName()}");
-        //    if (string.IsNullOrEmpty(propertyName))
-        //        throw new NullArgumentException("Property is empty or null, please provide a vaild property name", $"{GetCurrentMethodName()}");
-
-        //    //Object Mapping tree, hold the index of the properties and it's parents
-        //    Dictionary<Type, Dictionary<string, TrieNodeProperty>> propertyMappingIndex
-        //   = new Dictionary<Type, Dictionary<string, TrieNodeProperty>>();
-
-
-
-        //    //Property Trie - Make the top most root, doesn't have a value or a parent
-        //    TrieNodeProperty rootPropertyMap = new TrieNodeProperty(null, null);
-
-
-        //    TrieNodeProperty propertyNode = null;
-
-        //    propertyName = propertyName.ToUpper();
-        //    //Type of the source
-        //    var sourceType = source.GetType();
-
-        //    //Build the object mapping tree
-        //    LoadObjectDictionary(destination, propertyMappingIndex, rootPropertyMap);
-
-        //    //Get the dictionary by property type, this can still have other properties by source type
-        //    var propertyNodes = propertyMappingIndex[sourceType];
-
-        //    if (propertyNodes.Count > 1) //Check if there is more than one property of source type, if so get the one by the supplied name
-        //        propertyNode = propertyNodes[propertyName];
-        //    else
-        //        propertyNode = propertyNodes.Values.First(); //else get the first one
-
-        //    //Start mapping, auto memory initializationa and value assigning
-        //    StartAutoMappingProcess(destination, source, propertyNode, preserveExistingValue);
-
-        //}
-
-        ///// <summary>
-        ///// Copy over an Array, if the array already has items, add them to the array
-        ///// </summary>
-        ///// <param name="destinationData">Destination</param>
-        ///// <param name="SourceData">Source</param>
-        ///// <returns></returns>
+        /// <param name="destinationData">Destination</param>
+        /// <param name="SourceData">Source</param>
+        /// <returns></returns>
         private static object CopyOverArray(object destinationData, object SourceData)
         {
             var destinationList = destinationData as Array;
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AutoMapper
-This is a library that can map an object to another object. Currently this can map up to 3 layers deep.
+Mapping a part of object to another complex object becomes a tedious process.
+This is an auto mapper solution that can be used to solve this problem.
+
+### Features
+  * Automatically finds the first occurance of a object type and maps them.
+  * If there are multiple object of same type in the target object, you can specify the propety name to map it to.
+  * If the object already has some data in it, this will preserve the existing once and map only the new once.
+  * If target propert is `null`, the automapper can initialize the property and map the values.
+  * If the target property is a `List` or an `Array[]`, it can initilize if the property is `null` or if the it already has values the new values get added to it.
+
